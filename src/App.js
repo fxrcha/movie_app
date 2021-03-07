e87dd15f326fc25179fdd75035c47dbd1db4768d
HyunCha <truetype24@gmail.com>
2021-03-07 13:25:13 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -4,33 +4,38 @@ import Potato from './Potato';
 const albumLikes = [
   {
     id: 1,
+    rating: 10,
     name: "킁",
     image: "http://image.yes24.com/momo/TopCate3054/MidCate005/305340040.jpg"
   },
   {
     id: 2,
+    rating: 7,
     name: "SOKoNYun",
     image: "https://image.fmkorea.com/files/attach/new/20190820/486616/313058201/2107833995/32d96f894fc055559270ecf57ac6de77.jpg"
   },
   {
     id: 3,
+    rating: 9,
     name: "Stranger",
     image: "http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/160/480/81160480_1550223311331_1_600x600.JPG"
   },
   {
     id: 4,
+    rating: 10,
     name: "불러",
     image: "http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/419/600/81419600_1586857308134_1_600x600.JPG"
   },
   {
     id: 5,
+    rating: 8,
     name: "TL",
     image: "http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/112/583/81112583_1539333248976_1_600x600.JPG"
   }
 ];
 
 const renderAlbum = (album) => {
-  return <Potato key={album.id} name={album.name} image={album.image} />
+  return <Potato key={album.id} name={album.name} image={album.image} rating={album.rating} />
 }
 
 const App = () => {
--- src/Potato.js
+++ src/Potato.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-const Potato = ({ name, image }) => {
+const Potato = ({ name, image, rating }) => {
     return (
         <div>
             <h3>I like {name}!</h3>
+            <h4>Rating: {rating}</h4>
             <img src={image} style={{ width: 600 }} alt={name} />
         </div>
     )
 }
+
+Potato.propTypes = {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
+};
 
 export default Potato;
